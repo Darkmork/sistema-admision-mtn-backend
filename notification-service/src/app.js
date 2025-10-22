0af3d3ec170b37c3eb0f05d30b23b5c38481ae63
Darkmork <jorge.gangale@mail.udp.cl>
2025-10-22 09:45:03 -0300

    fix: Remove CORS headers from notification-service (handled by gateway)

--- notification-service/src/app.js
+++ notification-service/src/app.js
@@ -11,17 +11,8 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-// CORS configuration
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// CORS is handled by the API Gateway - microservices should not set CORS headers
+// The gateway's CORS configuration will be used for all responses
 
 // Request logging
 app.use((req, res, next) => {
