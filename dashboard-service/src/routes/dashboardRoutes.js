2260dafaddad2bb366f10921e000aa47f8630ad8
Darkmork <jorge.gangale@mail.udp.cl>
2025-11-11 23:37:34 -0300

    chore(dashboard): force redeploy v2 - overallOpinionScore

--- dashboard-service/src/routes/dashboardRoutes.js
+++ dashboard-service/src/routes/dashboardRoutes.js
@@ -7,7 +7,7 @@ const logger = require('../utils/logger');
 const cache = require('../config/cache');
 const { simpleQueryBreaker, mediumQueryBreaker, heavyQueryBreaker } = require('../config/circuitBreakers');
 
-// Force redeploy - 2025-11-11 18:20 - overallOpinionScore deployment
+// Force redeploy - 2025-11-11 18:25 - overallOpinionScore deployment v2
 /**
  * @route   GET /api/dashboard/stats
  * @desc    Get general dashboard statistics
